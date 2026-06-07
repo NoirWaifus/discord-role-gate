3d5f83e73a36c410ff526907089887445a6888db
NoirWaifus <noirnude.etsy@gmail.com>
2026-06-07 21:07:50 +0200

    Delete app/success directory

--- app/success/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SuccessPage() {
-  return (
-    <main style={{fontFamily:'Arial, sans-serif',padding:'40px',textAlign:'center'}}>
-      <h1>Todo correcto</h1>
-      <p>Ya tienes el rol en Discord.</p>
-    </main>
-  )
-}
